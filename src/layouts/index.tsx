c9eb32a7f5677cb819f3e8319c30cc494deb8e58
cheng <2683941980@qq.com>
2023-03-15 16:40:18 +0800

    init

---
+++ src/layouts/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link, Outlet } from 'umi';
+import styles from './index.less';
+
+export default function Layout() {
+  return (
+    <div className={styles.navs}>
+      <Outlet />
+    </div>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './index.less'
+
+export default function HomePage() {
+    return (
+        <div className={styles.page}>
+            <h1>
+                Genius
+            </h1>
+        </div>
+    );
+}
